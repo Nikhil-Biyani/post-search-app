5151ad50bc44d7641bcf17ca80dd2a8cfd05cdd0
Nikhil-Biyani <nikhilbiyani2406@gmail.com>
2024-06-03 04:14:43 +0530

    Added couple of comments

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -6,8 +6,8 @@ export const getPosts = (req: Request, res: Response) => {
     const searchParams = {
         query: query as string,
         sortBy: sortBy as string,
-        page: page ? parseInt(page as string, 10) : undefined,
-        pageSize: pageSize ? parseInt(pageSize as string, 10) : undefined,
+        page: page ? parseInt(page as string, 1) : undefined, // default as 1 if it is passed in the url
+        pageSize: pageSize ? parseInt(pageSize as string, 10) : undefined, // default as 10 if it is passed in the url
     }
 
     const result = searchPostsService(searchParams);
